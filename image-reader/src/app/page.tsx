ad415cee2d417a265ecaf869eb536076ab0458b7
AlexVLyon <vesterhus.lyon@gmail.com>
2025-03-16 18:39:22 +0100

    cleanup

--- image-reader/src/app/page.tsx
+++ image-reader/src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 import CameraReader from "./components/cameraReader";
-import { Typography, Container, Box, Link } from "@mui/material";
+import { Typography, Container, Box } from "@mui/material";
 
-export default function Home() {
+export default function CameraReaderPage() {
   return (
     <Container>
       <Box component="main" sx={{ my: 4 }}>
@@ -12,18 +11,6 @@ export default function Home() {
           </Typography>
           <CameraReader />
         </section>
-      </Box>
-      <Box component="footer" sx={{ py: 2, textAlign: 'center' }}>
-        <Link href="https://nextjs.org" target="_blank" rel="noopener noreferrer" underline="none">
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </Link>
       </Box>
     </Container>
   );
--- image-reader/src/app/receipt-reader/page.tsx
+++ image-reader/src/app/receipt-reader/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 import ReceiptReader from "../components/receiptReader";
-import { Typography, Container, Box, Link } from "@mui/material";
+import { Typography, Container, Box } from "@mui/material";
 
-export default function Home() {
+export default function ReceiptReaderPage() {
   return (
     <Container>
       <Box component="main" sx={{ my: 4 }}>
@@ -16,18 +15,6 @@ export default function Home() {
           </Typography>
           <ReceiptReader />
         </section>
-      </Box>
-      <Box component="footer" sx={{ py: 2, textAlign: 'center' }}>
-        <Link href="https://nextjs.org" target="_blank" rel="noopener noreferrer" underline="none">
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </Link>
       </Box>
     </Container>
   );
